f18221254ebf0854384972a0500c9a99e713f832
Angelina Korotkova <angelinakorotkova0401@mail.ru>
2023-10-18 12:45:48 +0300

    first commit popup

---
+++ client-state/popup/task.js
@@ -0,0 +1,14 @@
+const modal = document.getElementById('subscribe-modal');
+const closeButton = modal.querySelector('.modal__close');
+const isModalClosed = document.cookie.includes('modalClosed=true');
+
+
+if (!isModalClosed) {
+    modal.classList.add('modal_active');
+}
+const closeModal = () => {
+    modal.classList.remove('modal_active');
+    document.cookie = 'modalClosed=true';
+};
+
+closeButton.addEventListener('click', closeModal);
